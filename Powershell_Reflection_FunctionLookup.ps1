f48d5ea48de0f8c70eee4e753f3d4df17f77d739
FNGCrysis <johnv1202@gmail.com>
2021-07-10 14:31:27 -0700

    Create Powershell_Reflection_FunctionLookup.ps1

---
+++ Powershell_Reflection_FunctionLookup.ps1
@@ -0,0 +1,15 @@
+
+function LookupFunc($moduleName, $functionName) {
+
+  $Assemblies = [AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].Equals('System.dll') }
+  $unsafeObj = $systemdll.GetType('Microsoft.Win32.UnsafeNativeMethods')
+  $GetModuleHandleArray = $unsafeObj.GetMethods() | ForEach-Object {If ($_.Name -eq "GetModuleHandle") {$_} } 
+  $GetProcAddressArray = $unsafeObj.GetMethods() | ForEach-Object {If ($_.Name -eq "GetProcAddress") {$_}   }
+  $GetModuleHandle = $GetModuleHandleArray[0]
+  $GetProcAddress = $GetProcAddressArray[0]
+  $moduleHandle = $GetModuleHandle.Invoke($null, @($moduleName ))
+   
+  return $GetProcAddress.Invoke($null, @($moduleHandle, $functionName) )
+}
+
+LookupFunc "user32.dll" "MessageBoxA"
